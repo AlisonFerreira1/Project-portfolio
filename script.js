af4d20e729ef4dfb038bfe427b86b33bbca8d2ab
Alison.F <alisonqueiroz_@hotmail.com>
2025-02-05 17:41:52 -0300

    feature: Added navigation bar functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+function ocultarmenu() {
+  let menu = document.getElementById("menu");
+  let botao = document.getElementById("botaomenu");
+
+  // Alterna a classe "open" para abrir/fechar o menu
+  menu.classList.toggle("open");
+
+  // Se o menu está aberto, muda a cor do botão
+  if (menu.classList.contains("open")) {
+    botao.style.backgroundColor = "var(--secundary-color)"; // Cor quando aberto
+    botao.style.backgroundImage = 'url("../imgs/symbols_menu_black.svg")';
+  } else {
+    botao.style.backgroundImage = 'url("../imgs/symbols_menu_white.svg")';
+    botao.style.backgroundColor = "transparent"; // Cor quando fechado
+  }
+}
